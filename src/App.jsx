053df17410ad0d21aaf277b0c9b95c1ca3667cf1
ulisesdiaz049 <121645120+ulisesdiaz049@users.noreply.github.com>
2026-06-15 11:50:49 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -206,10 +206,10 @@ function AdminLogin({ onSuccess }) {
           onClick={handleLogin} 
           className="w-full bg-[#f43f5e] hover:bg-[#e11d48] text-white font-bold py-3 rounded-lg transition-colors mt-4 text-sm shadow-lg shadow-pink-500/20"
         >
-          Entrar al Dashboardulises
+          Entrar al Dashboard
         </button>
       </div>
-      <p className="text-[10px] text-slate-500 text-center mt-6">Credenciales demo: admin / 123456789</p>
+      {/*<p className="text-[10px] text-slate-500 text-center mt-6">Credenciales demo: admin / 123456789</p>*/}
     </div>
   );
 }
